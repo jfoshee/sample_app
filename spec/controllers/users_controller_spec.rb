42403e164d04db09521777a6ddb2857130e50f2f
Jacob Foshee <jacobf@gmail.com>
2010-11-14 12:29:39 -0600

    Finished layout and routes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe UsersController do
+  render_views
 
   describe "GET 'new'" do
     it "should be successful" do
@@ -9,4 +10,8 @@ describe UsersController do
     end
   end
 
+  it "should have the right title" do
+    get 'new'
+    response.should have_selector("title", :content => "Sign Up")
+  end
 end
