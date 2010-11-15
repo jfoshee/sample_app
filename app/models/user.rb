1c5e6cb8b1a1a2ffc5532d9cd1f7fb14d87281be
Jacob Foshee <jacobf@gmail.com>
2010-11-14 19:51:25 -0600

    uniqueness validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,11 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name, :presence => true,
+  validates :name,
+    :presence => true,
     :length => { :maximum => 50 }
-  validates :email, :presence => true,
-    :format => { :with => email_regex }
+  validates :email,
+    :presence => true,
+    :format => { :with => email_regex },
+    :uniqueness => { :case_sensitive => false }
 end
