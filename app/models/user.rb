7bd0cb1406547ae501c2fd6a4bff674309e4acfc
Jacob Foshee <jacobf@gmail.com>
2010-11-16 20:47:04 -0600

    added password and password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 # == Schema Information
-# Schema version: 20101114221622
+# Schema version: 20101117012421
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -26,4 +28,35 @@ class User < ActiveRecord::Base
     :presence => true,
     :confirmation => true,
     :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(password)
+  end
+
+  private
+  
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
 end
