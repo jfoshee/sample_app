7deccfafe9d2e76435592dd4ec5f6ab7d3c0c95b
Jacob Foshee <jacobf@gmail.com>
2010-12-12 17:00:18 -0600

    Done with sign in/out

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -40,4 +40,36 @@ describe "LayoutLinks" do
     click_link "Sign up now!"
     response.should have_selector('title', :content => "Sign")
   end
+
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path,
+        :content => "Sign in")
+    end
+
+    it "should not have a profile link" do
+      visit root_path
+      response.should_not have_selector("a", :content => "Profile")
+    end
+  end
+
+  describe "when signed in" do
+    before(:each) do
+      @user = Factory(:user)
+      integration_sign_in(@user)
+    end
+
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", :href => signout_path,
+        :content => "Sign out")
+    end
+    
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+        :content => "Profile")
+    end
+  end
 end
